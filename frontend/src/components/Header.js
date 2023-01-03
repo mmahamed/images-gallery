1a226c238dfe6a81f6e28132ff80a14e49e812fa
Mojtaba Mahamed <mmahamed@gmail.com>
2023-01-03 14:00:45 +0330

    SVG logo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,13 +1,18 @@
 import { Container, Navbar } from 'react-bootstrap';
+import { ReactComponent as Logo } from '../images/logo.svg';
 const navbarStyle = {
-  backgroundColor: 'lightblue',
+  backgroundColor: '#eee',
 };
 
 function Header({ title }) {
   return (
     <Navbar style={navbarStyle} variant="light">
       <Container>
-        <Navbar.Brand href="/">{title}</Navbar.Brand>
+        <Logo
+          alt={title}
+          title={title}
+          style={{ maxWidth: '25rem', maxHeight: '4rem' }}
+        />
       </Container>
     </Navbar>
   );
