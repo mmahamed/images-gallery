fa69c702a811041b4ae6564bb13748f4a22f2361
Mojtaba Mahamed <mmahamed@gmail.com>
2023-01-16 11:00:34 +0330

    basic images endpoint with get and post methods

--- api/main.py
+++ api/main.py
@@ -1,9 +1,15 @@
 """Main Flask App Module"""
 import os
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 from flask_cors import CORS
 import requests
+from mongo_client import mongo_client
+import json
+from bson import json_util
+
+gallery = mongo_client.gallery
+images_collection = gallery.images
 
 load_dotenv(dotenv_path="./.env.local")
 
@@ -35,6 +41,32 @@ def new_image():
     print(response)
     data = response.json()
     return data
+
+
+@app.route("/images", methods=["GET", "POST"])
+def images():
+    """View function for getting and posting images from database"""
+    if request.method == "GET":
+        # Read images from the database
+        images_from_db = images_collection.find({})
+        # we want to return an array of images
+
+        # res = json.loads(
+        #     json_util.dumps(images_from_db, json_options=json_util.DEFAULT_JSON_OPTIONS)
+        # )
+        # print(type(res)) #list
+        # return res
+
+        return jsonify([img for img in images_from_db])
+    if request.method == "POST":
+        # save image in the database
+        image = request.get_json()
+
+        # HERE WE RELY ON UNSPLASH ID
+        image["_id"] = image.get("id")
+        result = images_collection.insert_one(image)
+        inserted_id = result.inserted_id
+        return {"inserted_id": inserted_id}
 
 
 if __name__ == "__main__":
