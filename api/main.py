680f9c70e0857667aeb7911b01d1dd5070bf6081
Mojtaba Mahamed <mmahamed@gmail.com>
2023-01-17 12:51:15 +0330

    single image endpoint with delete method enabled

--- api/main.py
+++ api/main.py
@@ -54,7 +54,6 @@ def images():
         # )
         # print(type(res)) #list
         # return res
-
         return jsonify([img for img in images_from_db])
     if request.method == "POST":
         # save image in the database
@@ -66,6 +65,15 @@ def images():
         inserted_id = result.inserted_id
         return {"inserted_id": inserted_id}
 
+@app.route("/images/<image_id>", methods=["DELETE"])
+def image(image_id):
+    if request.method == "DELETE":
+        result = images_collection.delete_one({'_id': image_id})
+        if not result:
+            return {"error": "Image wasn't deleted. Please try again."}, 500
+        elif not result.deleted_count:
+            return {"error": "Image not found"}, 404
+        return {"deleted_id": image_id}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
