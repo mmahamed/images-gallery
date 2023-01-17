60c33b11f56b373dd01effc45861981eb55b0d86
Mojtaba Mahamed <mmahamed@gmail.com>
2023-01-17 13:35:01 +0330

    delete image request from the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -45,8 +45,16 @@ function App() {
     }
   };
 
-  const handleDeleteImage = (id) => {
-    setImages(images.filter((image) => image.id !== id));
+  const handleDeleteImage = async (id) => {
+    try {
+      const res = await axios.delete(`${API_URL}/images/${id}`);
+      if (res.status === 200) {
+        // OR if (res.data?.deleted_id)
+        setImages(images.filter((image) => image.id !== id));
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const handleSaveImage = async (id) => {
